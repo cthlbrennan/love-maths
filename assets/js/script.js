ff9d1706f7611442f9fa6fd4d59e871d68ccfc9b
Cathal Brennan <cthlbrennan@gmail.com>
2024-07-01 14:26:17 +0000

    Add code to calculateCorrectAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,12 @@ function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer() {}
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText)
+    let operand2 = parseInt(document.getElementById("operand2").innerText)
+    let operator = document.getElementById("operand1").innerText;
 
+}
 
 function incrementScore() {
 
