57cdb3552a88867622c171f2767143870faa2465
Cathal Brennan <cthlbrennan@gmail.com>
2024-07-01 14:47:28 +0000

    Add more checkAnswer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!")
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type")
                     runGame(gameType);
@@ -34,13 +34,30 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("Correct")
+    } else {
+        alert(`wrong. answer is ${calculatedAnswer[0]}, not ${userAnswer}`)
+    }
+
+    runGame(calculatedAnswer[1])
 
 }
 
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById("operand1").innerText)
     let operand2 = parseInt(document.getElementById("operand2").innerText)
-    let operator = document.getElementById("operand1").innerText;
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert(`unimplemented error ${operator}`);
+        throw `unimplemented error ${operator}. Aborting!`
+    }
 
 }
 
